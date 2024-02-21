82c046403ebb4f0b95c079d7dadcea53912d00c5
xiongxeng <xiongxeng@gmail.com>
2024-02-20 20:04:33 -0600

    add images, add about me sections, also link into nav bar will add more about the bio later about the author

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { useState } from "react"
 import NavBar from "./components/Navigation/NavBar";
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+
+
 
 
 
@@ -14,6 +17,7 @@ function App() {
         <div className ="">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </Router>
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import profileCover from '../assets/Images/avatar.png';
+import hmongCloth from '../assets/Images/hmongCloth.jpg';
+import LongXiong from '../assets/Images/Long.jpg';
+
+
+const About = () => {
+  return (
+    <div className="">
+      <div className="pt-10 lg:mx-[15%]">
+        <div>
+          <div className="h-30 w-30">
+            <img
+              className=" h-[55vh] w-full object-cover "
+              src={hmongCloth}
+              alt="Hmong Cloth"
+            />
+          </div>
+
+          <div className=" flex justify-between items-center ">
+            <div className="bg-white shadow-lg p-1 h-20 lg:h-32 w-20 lg:w-32 rounded-lg -mt-28 ml-5 ">
+              <img
+                className=" h-full w-full object-cover rounded-lg"
+                src={LongXiong}
+                alt="LongXiong"
+              />
+            </div>
+
+          </div>
+          <div className=" ml-5">
+            <h1 className=" text-pbl font-bold text-2xl  my-2">Nao Long Xiong</h1>
+
+            <p className=" text-xl">Hmong Xiong Elder</p>
+
+            <p className=" text-tbl mt-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+          tempor incididunt ut labore et dolore magna aliqua. Nisl nunc mi ipsum 
+          faucibus vitae aliquet. Arcu bibendum at varius vel pharetra vel turpis. 
+          Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a. 
+          Ac orci phasellus egestas tellus. Nunc mi ipsum faucibus vitae aliquet. 
+          Ultrices eros in cursus turpis massa tincidunt dui. Ultrices eros in cursus 
+          turpis massa. Eget lorem dolor sed viverra ipsum nunc. Ullamcorper morbi 
+          tincidunt ornare massa eget egestas purus. Risus viverra adipiscing at in. 
+          Purus sit amet luctus venenatis lectus magna fringilla urna porttitor. 
+          Adipiscing elit ut aliquam purus sit amet luctus. Nunc eget lorem dolor 
+          sed viverra ipsum nunc aliquet.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
